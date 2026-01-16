2ebd04c2255d328e23b35e531731f6036477203b
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-16 17:10:15 +0530

    Prepare for Render deployment

--- blogsite/blogsite/settings.py
+++ blogsite/blogsite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(_(5-=(6j)+&m8#!tg9%i8$w(sq5e^-)@pc77hf8!ks+v$($-n'
+SECRET_KEY = os.getenv("django-insecure-(_(5-=(6j)+&m8#!tg9%i8$w(sq5e^-)@pc77hf8!ks+v$($-n")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,16 +73,11 @@ WSGI_APPLICATION = 'blogsite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DailyLog',
-        'USER':'postgres',
-        'PASSWORD':'pratik1642',
-        'HOST':'localhost',
-    }
+    'default': dj_database_url.config(conn_max_age=600)
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
