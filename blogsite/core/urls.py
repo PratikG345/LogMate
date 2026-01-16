e8454943c8c2fc7adc56ec69196d28097ed3b383
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-16 16:43:04 +0530

    log_list

--- blogsite/core/urls.py
+++ blogsite/core/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import home,list,update_log,delete_log,create_log,log_detail
+from .views import home,log_list,update_log,delete_log,create_log,log_detail
 urlpatterns = [
     path('',home,name='home'),
-    path('list/',list,name='list'),
+    path('list/',log_list,name='log_list'),
     path('list/<int:pk>',log_detail,name='log_detail'),
     path('add/',create_log,name='create_log'),
     path('list/<int:pk>/',update_log,name="update_log"),
