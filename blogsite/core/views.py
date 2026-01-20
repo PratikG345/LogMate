12b0a635646ac5be41b0985af3c8b28f309cda5f
Pratik Atul Gaikwad <mrpratikonline@gmail.com>
2026-01-20 11:05:47 +0530

    added pagination

--- blogsite/core/views.py
+++ blogsite/core/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import Log
+from django.core.paginator import Paginator
 from django.utils.timezone import now
 from datetime import timedelta
 from .forms import Form
@@ -21,7 +22,18 @@ def log_list(req):
     elif filter_type == "30days":
         logs = logs.filter(date__gte = today - timedelta(days=30))    
     
-    return render(req,"core/list.html",{"logs":logs})
+    paginator = Paginator(logs, 10)  # 10 logs per page
+    page_number = req.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        req,
+        "core/list.html",
+        {
+            "page_obj": page_obj,
+            "filter_type": filter_type,
+        }
+    )
 
 def log_detail(req,pk):
     logs = Log.objects.get(pk=pk)
